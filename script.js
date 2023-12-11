0f27cd7282c1d7485c522bf731c1e2fd09e69f82
Pratik Dhimal <127828800+Pratik07007@users.noreply.github.com>
2023-12-11 10:54:35 +0545

    Add files via upload
    Script file uploaded

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.from(".logo , .navbar-options , .nav-right",{
+    y:-100,
+    opacity:0,
+    delay:0.1,
+    duration:1,
+    scale:0,
+    stagger:0.1,
+    // rotate:20
+})
+gsap.from(".dabba h1, .dabba a",{
+    opacity:0,
+    delay:0.1,
+    duration:1,
+    scale:0,
+    stagger:1,
+    rotate:20
+})
+
